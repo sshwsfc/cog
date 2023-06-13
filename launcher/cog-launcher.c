02aa7e72bdaaaa018e5f32587d2f05c6ff55f5c2
Arnaud Vrac <avrac@freebox.fr>
2023-06-13 16:44:32 +0200

    launcher: set preferred language list from system locale

--- launcher/cog-launcher.c
+++ launcher/cog-launcher.c
@@ -270,6 +270,8 @@ on_automation_started(WebKitWebContext *context, WebKitAutomationSession *sessio
 static void
 cog_launcher_startup(GApplication *application)
 {
+    const gchar * const *locales;
+
     G_APPLICATION_CLASS(cog_launcher_parent_class)->startup(application);
 
     /*
@@ -317,6 +319,24 @@ cog_launcher_startup(GApplication *application)
     g_clear_pointer(&s_options.web_extensions_dir, g_free);
 
     g_object_set(self->shell, "device-scale-factor", s_options.device_scale_factor, NULL);
+
+    /* build Accept-Language from locale, filtering out encodings */
+    locales = g_get_language_names ();
+    if (locales) {
+        guint i, n, length = g_strv_length ((gchar **)locales);
+        gchar ** languages = g_new0 (gchar *, length);
+
+        for (n = 0, i = 0; locales[i]; i++) {
+            const gchar *lang = locales[i];
+            if (!strcmp(lang, "C") || !strcmp(lang, "POSIX") || strchr(lang, '.'))
+                continue;
+            languages[n++] = g_strdup(lang);
+        }
+
+        webkit_web_context_set_preferred_languages(cog_shell_get_web_context(self->shell),
+                                                   (const gchar *const *) languages);
+        g_strfreev(languages);
+    }
 
     if (s_options.handler_map) {
         GHashTableIter i;
